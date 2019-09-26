4aad34a96a7ce9443d81e32fd623b4b0421098e1
aglegaspi <lex83nyc@gmail.com>
2019-09-26 13:40:55 -0400

    first commit

---
+++ iOS_Collection_View_Lab/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_Collection_View_Lab
+//
+//  Created by Alexander George Legaspi on 9/26/19.
+//  Copyright © 2019 Alexander George Legaspi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
